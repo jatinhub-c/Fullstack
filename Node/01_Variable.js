cea6b9b9c0c090367bbdd17b1a21db4e62f7a462
jatinhub-c <ss8092457@gmail.com>
2026-01-01 05:58:58 +0000

    variabele first change

---
+++ Node/01_Variable.js
@@ -0,0 +1,15 @@
+// var - prefer not to use var , beacuse of issiue in block scope and functional scope.
+// let 
+// const
+
+const accountId = 67; // velue can't be changed on another place by using const.
+let accountEmail= "ss8092457@gmail.com"
+var accountPassword = "jklmnn"
+acountCity = "Mathura"
+
+// accountId= 69; not allowed
+
+console.log(accountId);
+
+console.table([accountId ,accountEmail,accountPassword,acountCity])
+
