1f83dedd5436c616332d7b140d651a35a0c69a7a
jatinhub-c <ss8092457@gmail.com>
2026-01-12 16:50:28 +0000

    second commit

--- Node/Basics_03/02_scopes.js
+++ Node/Basics_03/02_scopes.js
@@ -26,3 +26,33 @@ function one(){
      two()
 
 }
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
