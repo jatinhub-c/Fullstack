eba213f6794aab59766e6188f468b5da7950e852
jatinhub-c <ss8092457@gmail.com>
2026-01-12 16:46:06 +0000

    second commit

--- Node/Basics_03/functions.js
+++ Node/Basics_03/functions.js
@@ -26,3 +26,31 @@ const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
 
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
